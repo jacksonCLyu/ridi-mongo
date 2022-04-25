56adcbe9f6a66b6569333c4d92f416811edb3d47
young <yucl930904@163.com>
2022-04-25 15:38:39 +0800

    mongo service

---
+++ mongoserve/factory.go
@@ -0,0 +1,17 @@
+package mongoserve
+
+import (
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// GetClient returns a mongo client
+func GetClient(hostStr string) *mongo.Client {
+	return GetClientWithOptions(hostStr, DefaultOptions())
+}
+
+// GetClientWithOptions returns a mongo client with options
+func GetClientWithOptions(serveName string, options *options.ClientOptions) *mongo.Client {
+	client, _ := mongoClientMap.LoadOrStore(serveName, InitPool(options))
+	return client.(*mongo.Client)
+}
